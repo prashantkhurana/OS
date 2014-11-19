b1a6de96021779ad3a56450bcf73edad5f580934
Prashant <prashant.khurana@cs.rutgers.edu>
2014-11-19 00:26:44 -0500

    imple left

--- defs.h
+++ defs.h
@@ -118,6 +118,8 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+int             cowfork(void);
+
 
 // swtch.S
 void            swtch(struct context**, struct context*);
--- user.h
+++ user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int cowfork(void);
 
 // ulib.c
 int stat(char*, struct stat*);
