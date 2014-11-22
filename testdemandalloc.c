5f944911bb424eb005ea98a6bfa2628e09cb91a6
Prashant <prashant.khurana@cs.rutgers.edu>
2014-11-21 19:27:33 -0500

    done 2

--- testdemandalloc.c
+++ testdemandalloc.c
@@ -7,39 +7,51 @@
 int
 main(int argc, char *argv[])
 {
-  int num=10000;
+int num=10000;
 void * addr;
+
 int i;
-printf(1,"%d\n ",uptime());  
+
+
+
+int dsbrk_alloc_time,dsbrk_write_time;
+int before,after, after2;
+printf(1,"dsbrk started\n");
 for(i=0;i<num;i++)
 {
 //printf(1,"%d ",i);  
+before=uptime();
 addr=dsbrk(4096);
-*((int *)addr)=10;
+after=uptime();
+dsbrk_alloc_time+=(after-before);
+
+memset(addr,0,4096);
+after2=uptime();
+dsbrk_write_time+=(after2-after);
+
 }
-printf(1,"%d\n ",uptime());  
-printf(1,"dsbrk done");
 
+printf(1,"dsbrk done\n");
 
+printf(1,"sbrk started\n");
 
-printf(1,"%d\n ",uptime());  
+int sbrk_alloc_time,sbrk_write_time;
 for(i=0;i<num;i++)
 {
 //printf(1,"%d ",i);  
+before=uptime();
 addr=sbrk(4096);
-*((int *)addr)=10;
-}
-printf(1,"%d\n ",uptime());  
-printf(1,"sbrk done");
+after=uptime();
+sbrk_alloc_time+=(after-before);
+memset(addr,0,4096);
+after2=uptime();
+sbrk_write_time+=(after2-after);
 
-//int len = 4096;
-//printf(1,"%d",mpro(addr, len,PROT_READ));
-// for(i=0;i<num;i++)
-// {
-// 
-// *((int *)addr[i])=10;
-// }
-printf(1,"assignment done");
-//printf(1,"%d",uptime());
+}
+
+
+printf(1,"sbrk done\n");
+
+printf(1,"DSBRK ALLOC TIME:%d\nSBRK ALLOC TIME:%d\nDSBRK WRITE TIME:%d\nSBRK WRITE TIME:%d\n",dsbrk_alloc_time,sbrk_alloc_time,dsbrk_write_time,sbrk_write_time);
 exit();
 }
